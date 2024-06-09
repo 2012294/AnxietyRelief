2d25a4a7d2e97b1efb32e72b3ad247d62f10563a
2012294 <109684404+2012294@users.noreply.github.com>
2024-06-10 00:56:32 +0500

    Update RetrofitClientInstance.java

--- app/src/main/java/com/hareem/anxietyrelief/RetrofitClientInstance.java
+++ app/src/main/java/com/hareem/anxietyrelief/RetrofitClientInstance.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http:192.168.100.250:5000";
+    private static final String BASE_URL = "http:your_IPV4:5000";
 
 
 
